22ff97c81d3aa80a08f763e4a0278707cc0e03c8
Lopes, Maycon <lopes.maycon@gmail.com>
2025-03-21 11:16:45 -0300

    feat(User relationship)

--- src/main/java/com/mike/dscommerce/entities/User.java
+++ src/main/java/com/mike/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.mike.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +18,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -76,5 +81,8 @@ public class User {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
 
 }
